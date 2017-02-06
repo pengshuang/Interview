df0a1d51e56aeb737527b34b7ca6a396ff8d33fb
pengshuang <pengshuang92@163.com>
2017-02-06 16:14:30 +0800

    二叉树问题总结

--- src/interview/BinaryReeSummary.java
+++ src/interview/BinaryReeSummary.java
@@ -31,8 +31,8 @@ public class BinaryReeSummary {
         r2.right = r5;
         r3.right = r6;
 
-        int count = getNodeNumKthLevelRec(r1, 2);
-        System.out.print(count);
+        TreeNode count = getLastCommonParentRec(r1, r4, r5);
+        System.out.print(count.val);
     }
 
     private static class TreeNode {
@@ -450,5 +450,30 @@ public class BinaryReeSummary {
             }
         }
         return newRoot;
+    }
+
+    public static TreeNode getLastCommonParentRec(TreeNode root, TreeNode n1, TreeNode n2) {
+        if(root == null){
+            return null;
+        }
+
+        // 如果有一个match，则说明当前node就是要找的最低公共祖先
+        if(root.equals(n1) || root.equals(n2)){
+            return root;
+        }
+        TreeNode commonInLeft = getLastCommonParentRec(root.left, n1, n2);
+        TreeNode commonInRight = getLastCommonParentRec(root.right, n1, n2);
+
+        // 如果一个左子树找到，一个在右子树找到，则说明root是唯一可能的最低公共祖先
+        if(commonInLeft!=null && commonInRight!=null){
+            return root;
+        }
+
+        // 其他情况是要不然在左子树要不然在右子树
+        if(commonInLeft != null){
+            return commonInLeft;
+        }
+
+        return commonInRight;
     }
 }
