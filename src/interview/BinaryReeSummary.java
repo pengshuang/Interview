12f5e6ecdc025d5e078bb26dcb8fbd76b25eaed2
pengshuang <pengshuang92@163.com>
2017-02-06 16:07:22 +0800

    二叉树问题总结

--- src/interview/BinaryReeSummary.java
+++ src/interview/BinaryReeSummary.java
@@ -353,4 +353,102 @@ public class BinaryReeSummary {
         }
         return isAVLRec(root.left) && isAVLRec(root.right);
     }
+
+    /**
+     * 求二叉树的镜像 递归解法：
+     * （1）如果二叉树为空，返回空
+     * （2）如果二叉树不为空，求左子树和右子树的镜像，然后交换左子树和右子树
+     */
+    // 不破坏原来的树
+    public static TreeNode mirrorRec(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode left = mirrorRec(root.left);
+        TreeNode right = mirrorRec(root.right);
+
+        root.left = right;
+        root.right = left;
+        return right;
+    }
+    // 破坏原来的树
+    public static TreeNode mirrorCopyRec(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode newNode = new TreeNode(root.val);
+        newNode.left = mirrorCopyRec(root.right);
+        newNode.right = mirrorCopyRec(root.left);
+
+        return newNode;
+    }
+
+    // 3. 判断两个树是否互相镜像
+    public static boolean isMirrorRec(TreeNode r1, TreeNode r2){
+        // 如果两个树都是空树，则返回true
+        if(r1==null && r2==null){
+            return true;
+        }
+
+        // 如果有一棵树是空树，另一颗不是，则返回false
+        if(r1==null || r2==null){
+            return false;
+        }
+
+        // 如果两个树都非空树，则先比较根节点
+        if(r1.val != r2.val){
+            return false;
+        }
+
+        // 递归比较r1的左子树的镜像是不是r2右子树 和
+        // r1的右子树的镜像是不是r2左子树
+        return isMirrorRec(r1.left, r2.right) && isMirrorRec(r1.right, r2.left);
+    }
+
+    public static void mirror(TreeNode root) {
+        if(root == null){
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode cur = stack.pop();
+            TreeNode tmp = cur.right;
+            cur.right = cur.left;
+            cur.left = tmp;
+
+            if(cur.right != null){
+                stack.push(cur.right);
+            }
+            if(cur.left != null){
+                stack.push(cur.left);
+            }
+        }
+    }
+
+    public static TreeNode mirror2(TreeNode root){
+        if (root == null){
+            return null;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<TreeNode> newStack = new Stack<>();
+        stack.push(root);
+        TreeNode newRoot = new TreeNode(root.val);
+        newStack.push(newRoot);
+
+        while (!stack.isEmpty()){
+            TreeNode cur = stack.pop();
+            TreeNode newCur = newStack.pop();
+
+            if(cur.right != null){
+                stack.push(cur.right);
+                newCur.left = new TreeNode(cur.right.val);
+                newStack.push(newCur.left);
+            }
+            if(cur.left != null){
+                stack.push(cur.left);
+                newCur.right = new TreeNode(cur.left.val);
+                newStack.push(newCur.right);
+            }
+        }
+        return newRoot;
+    }
 }
