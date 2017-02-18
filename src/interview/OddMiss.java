e678b0da874e7867500a57b51a3ef9d40f08c3af
pengshuang <pengshuang92@163.com>
2017-02-18 09:19:37 +0800

    奇数位丢失

--- src/interview/OddMiss.java
+++ src/interview/OddMiss.java
@@ -6,16 +6,18 @@ import java.util.Scanner;
 public class OddMiss {
     public static void main(String [] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            list.add(i);
-        }
-        while (list.size() != 1){
-            for (int i = 0; i < list.size(); i++) {
-                list.remove(i);
+        while (in.hasNext()){
+            int n = in.nextInt();
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int i = 0; i <= n; i++) {
+                list.add(i);
             }
+            while (list.size() != 1){
+                for (int i = 0; i < list.size(); i++) {
+                    list.remove(i);
+                }
+            }
+            System.out.println(list.get(0));
         }
-        System.out.println(list.get(0));
     }
 }
