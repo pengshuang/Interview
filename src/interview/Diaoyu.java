87e5719cd00b52f7a1c33675cd49d71875c304f9
pengshuang <pengshuang92@163.com>
2017-02-18 15:50:09 +0800

    钓鱼问题

---
+++ src/interview/Diaoyu.java
@@ -0,0 +1,41 @@
+package interview;
+
+import java.util.*;
+
+public class Diaoyu {
+    public static void main(String [] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()){
+            int n = in.nextInt();
+            int m = in.nextInt();
+            int x = in.nextInt() - 1;
+            int y = in.nextInt() - 1;
+            int t = in.nextInt();
+            in.nextLine();
+            double [][] p = new double[n][m];
+            for (int i = 0; i < n; i++) {
+                String[] content = in.nextLine().split(" ");
+                for (int j = 0; j < m; j++) {
+                    p[i][j] = Double.parseDouble(content[j]);
+                }
+            }
+            double count = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    count += p[i][j];
+                }
+            }
+            double avg = count / (n * m);
+            if (p[x][y] > avg){
+                System.out.println("cc");
+                System.out.println(String.format("%.2f", 1 - Math.pow(1 - p[x][y], t)));
+            } else if (p[x][y] < avg){
+                System.out.println("s");
+                System.out.println(String.format("%.2f", 1 - Math.pow(1 - avg, t)));
+            } else {
+                System.out.println("equal");
+                System.out.println(String.format("%.2f", 1 - Math.pow(1 - avg, t)));
+            }
+        }
+    }
+}
