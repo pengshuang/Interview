7e505abf89129afcfb56bb6eb106587e8585a927
pengshuang <pengshuang92@163.com>
2017-02-18 09:16:52 +0800

    奇数位丢失

---
+++ src/interview/OddMiss.java
@@ -0,0 +1,21 @@
+package interview;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class OddMiss {
+    public static void main(String [] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            list.add(i);
+        }
+        while (list.size() != 1){
+            for (int i = 0; i < list.size(); i++) {
+                list.remove(i);
+            }
+        }
+        System.out.println(list.get(0));
+    }
+}
